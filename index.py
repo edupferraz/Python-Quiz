24152830c252b04a40518394748d3f9d72b15b63
Eduardo Ferraz <edupferraz@gmail.com>
2023-06-22 09:32:56 +0000

    Finish the project

--- index.py
+++ index.py
@@ -23,6 +23,7 @@ perguntas = [
         }
     },
 
+
     {
         "pergunta": "Prefere dano mágico ou dano físico?",
         "opcoes": [
@@ -37,6 +38,7 @@ perguntas = [
         }
     },
 
+
     {
         "pergunta": "A primeira escolha de campeão seria?",
         "opcoes": [
@@ -48,22 +50,182 @@ perguntas = [
             "a": {"Jax", "K'sante", "Graves", "Lee Sin", "Kayn", "Yasuo", "Zed", "Jhin", "Ezreal", "Milio", "Thresh"},
             "b": {"Kayle", "Ahri", "Katarina", "Jinx", "Caitlyn", "Kaisa"},
             "c": {"Malphite", "Ornn", "Kha'Zix", "Hecarim", "Aurelion Sol", "Lulu", "Yuumi", "Nautilus", }
-        },
+        }
     },
 
+
+    {
+        "pergunta": "De qual região você seria?",
+        "opcoes": [
+            "a) Bandópolis",
+            "b) Demacia",
+            "c) Freljord",
+            "d) Ilha das Sombras",
+            "e) Ionia",
+            "f) Ixtal",
+            "g) Noxus",
+            "h) Vazio",
+            "i) Piltover",
+            "j) Shurima",
+            "k) Targon",
+            "l) Zaun",
+            "m) Águas de Sentina",
+            "n) Runeterra"
+
+
+        ],
+        "pontuacao": {
+            "a": {"Lulu", "Yuumi"},
+            "b": {"Kayle"},
+            "c": {"Ornn"},
+            "d": {"Hecarim", "Thresh"},
+            "e": {"Ahri", "Jhin", "Kayn", "Lee Sin", "Yasuo", "Zed"},
+            "f": {"Malphite", "Milio"},
+            "g": {"Katarina"},
+            "h": {"Kaisa", "Kha'Zix"},
+            "i": {"Caitlyn", "Ezreal"},
+            "j": {"K'sante"},
+            "k": {"Aurelion Sol"},
+            "l": {"Jinx"},
+            "m": {"Graves", "Nautilus"},
+            "n": {"Jax"}
+        }
+    },
+
+
+    {
+        "pergunta": "Qual elemento te define?",
+        "opcoes": [
+            "a) Água",
+            "b) Fogo",
+            "c) Terra",
+            "d) Ar",
+            "e) Luz",
+            "f) Escuridão"
+        ],
+        "pontuacao": {
+            "a": {"Jhin", "Milio", "Nautilus"},
+            "b": {"Ornn", "Graves", "Jinx", "Caitlyn"},
+            "c": {"Malphite", "K'sante", "Lee Sin", "Yasuo"},
+            "d": {"Ahri", "Aurelion Sol", "Katarina", "Yuumi"},
+            "e": {"Kayle", "Ezreal", "Lulu"},
+            "f": {"Jax", "Kha'Zix", "Hecarim", "Kayn", "Zed", "Kaisa", "Thresh"}
+        }
+    },
+
+
+    {
+        "pergunta": "O que você prefere?",
+        "opcoes": [
+            "a) Matar todo mundo sozinho",
+            "b) Lutar com o meu time",
+            "c) Um pouco de cada",
+            "d) Ser uma ward humana"
+
+
+        ],
+        "pontuacao": {
+            "a": {"Jax", "Kha'Zix", "Yasuo", "Zed", "Jinx"},
+            "b": {"Malphite", "Ornn", "Hecarim", "Ahri", "Aurelion Sol", "Jhin", "Ezreal", "Caitlyn", "Yuumi", "Thresh", "Nautilus"},
+            "c": {"Kayle", "K'sante", "Lee Sin", "Graves", "Kayn", "Kaisa", "Katarina"},
+            "d": {"Lulu", "Milio"}
+
+
+        }
+    },
+
+
+    {
+        "pergunta": "Relacionado a dificuldade, você prefere os campeões que sejam",
+        "opcoes": [
+            "a) Simples e brutal",
+            "b) Nem fácil, nem difícil",
+            "c) Complexo, que poucas pessoas sabem jogar"
+
+
+        ],
+        "pontuacao": {
+            "a": {"Malphite", "Kha'Zix", "Ornn", "Graves", "Ahri", "Aurelion Sol", "Jinx", "Kaisa", "Lulu", "Yuumi"},
+            "b": {"K'sante", "Jax", "Kayle", "Hecarim", "Katarina", "Jhin", "Ezreal", "Caitlyn", "Milio"},
+            "c": {"Yasuo", "Zed", "Lee Sin", "Thresh", "Kayn", "Nautilus"}
+
+
+        }
+    },
+
+
+    {
+        "pergunta": "Qual estilo de jogo você prefere?",
+        "opcoes": [
+            "a) Ganhar de virada",
+            "b) Empurrar as rotas rapidamente",
+            "c) Jogar com poke",
+            "d) Ter uma composição forte"
+
+
+        ],
+        "pontuacao": {
+            "a": {"Katarina", "Kha'Zix", "Graves", "Kaisa", "Yuumi"},
+            "b": {"Ornn", "Jax", "Kayle", "Kayn", "Yasuo", "Zed", "Jinx", "Lulu"},
+            "c": {"Hecarim", "Ahri", "Ezreal", "Milio"},
+            "d": {"Malphite", "K'sante", "Lee Sin", "Aurelion Sol", "Jhin", "Caitlyn", "Nautilus", "Thresh"}
+
+
+        }
+    },
+
+
+    {
+        "pergunta": "Flash no D ou no F?",
+        "opcoes": [
+            "a) D",
+            "b) F",
+            "c) Tanto faz (psicopata)"
+
+
+        ],
+        "pontuacao": {
+            "a": {"Ornn", "Kha'Zix", "Lee Sin", "Hecarim", "Ahri", "Katarina", "Caitlyn", "Kaisa", "Milio", "Thresh"},
+            "b": {"Kayle", "K'sante", "Yasuo", "Graves", "Aurelion Sol", "Ezreal", "Jhin", "Lulu", "Nautilus"},
+            "c": {"Malphite", "Jax", "Kayn", "Zed", "Jinx", "Yuumi"}
+
+
+        }
+    },
+
+
+    {
+        "pergunta": "Você gosta de disparos de precisão?",
+        "opcoes": [
+            "a) Prefiro campeões cheios deles",
+            "b) Gosto mais de skills de clicar",
+            "c) Os dois"
+
+
+        ],
+        "pontuacao": {
+            "a": {"Graves", "Lee Sin", "Ahri", "Jhin", "Caitlyn", "Ezreal", "Thresh"},
+            "b": {"Malphite", "Jax", "Ornn", "K'sante", "Kha'Zix", "Kayn", "Hecarim", "Katarina", "Aurelion Sol", "Milio", "Lulu", "Yuumi"},
+            "c": {"Kayle", "Yasuo", "Zed", "Jinx", "Kaisa", "Nautilus"}
+
+
+        }
+    },
 ]
 
 respostas_jogador = []
 
 for pergunta in perguntas:
-    print(pergunta["pergunta"])
+    print("\n" + pergunta["pergunta"])
     for opcao in pergunta["opcoes"]:
         print(opcao)
-    resposta = input("Digite a letra correspondente à sua resposta: ")
+    resposta = input("\nDigite a letra correspondente à sua resposta: ")
     respostas_jogador.append(resposta)
 
 # Calcular a pontuação com base nas respostas
 pontuacao = {}
+max_score = 0
+best_champion = ""
 
 for i, resposta in enumerate(respostas_jogador):
     opcao = perguntas[i]["pontuacao"].get(resposta)
@@ -75,6 +237,17 @@ for i, resposta in enumerate(respostas_jogador):
                 pontuacao[campeao] = 1
 
 # Exibir a pontuação final
-print("Pontuação Final:")
+
 for campeao, pontos in pontuacao.items():
     print(campeao + ": " + str(pontos))
+
+    if pontos > max_score:
+        max_score = pontos
+        best_champion = campeao
+
+if best_champion:
+    print("\nCampeão com maior pontuação: " + best_champion)
+    print("\nVeja seu campeão: " + "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" +
+          best_champion+"_0.jpg")
+else:
+    print("\nCampeão com maior pontuação: Amumu")
